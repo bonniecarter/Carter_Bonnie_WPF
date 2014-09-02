d043f7acc34d359882866e76db1e52b5be58abf6
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-02 15:56:42 -0400

    Prompted user for name
    Prompted user for name

--- Setup GitHub/js/script.js
+++ Setup GitHub/js/script.js
@@ -4,4 +4,10 @@ Test JS file
 090214
  */
 
-//alert("Testing 1,2,3");
+//alert("Testing 1, 2, 3");
+
+
+console.log("Testing the console.log");
+
+var userInput = prompt("Type in your name:");
+console.log(userInput):
