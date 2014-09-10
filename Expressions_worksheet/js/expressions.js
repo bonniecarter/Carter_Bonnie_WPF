0328244fcb31e9b00bd611dc054b518456bf3ae0
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-10 17:27:15 -0400

    dog years
    i have created the function for how old is your dog.

--- Expressions_worksheet/js/expressions.js
+++ Expressions_worksheet/js/expressions.js
@@ -8,4 +8,12 @@
 var name = prompt("What is your dog\'s name?");
 
 //test to make sure this works
-console.log(name)
+console.log(name);
+
+//how old is their dog?
+var age = prompt("How old is your dog?");
+
+var dogAge = age*7;
+
+//alert the user with the dogs age in human age
+alert(name+" is "+dogAge+"in human years")
