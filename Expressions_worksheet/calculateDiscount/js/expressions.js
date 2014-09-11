bf5bf24fb9492586c627cac9a65e1995b80cf838
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-11 18:01:43 -0400

    discounts
    created new directory for discounts

---
+++ Expressions_worksheet/calculateDiscount/js/expressions.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Bonnie Carter
+ * Sept 10 2014
+ * Activity: Expressions Worksheet
+ */
+
+//What are we asking for:
+//alert("Do you know how old your dog is in doggy years versus human years? We do!");
+
+//Getting Dog's Age:
+//var name = prompt("What is your dog\'s name?");
+
+//test to make sure this works
+//console.log(name);
+
+//how old is their dog?
+//var age = prompt("How old is your dog?");
+
+//var dogAge = age*7;
+
+//alert the user with the dogs age in human age
+//alert(name+" is "+age+" in human years which equates to "+dogAge+" in doggy years!")
+
+//prompt for input of original price, discount, description & sales tax
+alert("Thank you for considering our lip gloss, find out what the sale price is here.");
