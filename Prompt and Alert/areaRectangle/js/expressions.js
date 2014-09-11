b3414d6459787167ff82fc0bbca63adbf96fff2e
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-11 19:03:41 -0400

    Prompt & Alert
    created file from copy of dog assignment and edited information

---
+++ Prompt and Alert/areaRectangle/js/expressions.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Bonnie Carter
+ * Sept 11 2014
+ * Activity: Prompts & Alert
+ */
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height");
+var area = width * height; //calculates with that info
+//console.log(area); //prints it out to console
+var result = "The area of your rectangle is " + area + " sq feet";
+alert(result);
