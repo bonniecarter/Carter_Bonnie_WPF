b8eda98d9caa6fa9eac2999fa7e54fb814323515
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-11 20:26:04 -0400

    prompts
    added prompts to get data for project

---
+++ Expressions_Wacky/Expressions_Wacky/js/expressions.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Bonnie Carter
+ * Sept 11 2014
+ * Activity: Wacky Expressions
+ */
+
+//explain the project
+alert("Wondering what that storage locker is worth at an auction?");
+
+var lockerWidth = prompt("What is the width of the locker?");
+var lockerHeight = prompt("What is the height of the locker?");
+var lockerLength = prompt("What is the length of the locker?");
+
+//calculate the locker size
+
+var cubicFeet = lockerHeight*lockerLength*lockerWidth;
+
+//prompt for percentage full
+
+var percentFull = prompt("What percentage of the locker is full (not empty)?");
+
+//prompt for black garbage bags
+
+var garbageBags = prompt("How many black garbage bags do you see?");
+
+//prompt for mystery boxes
+
+var mysteryBoxes = prompt("How many mystery boxes are there?");
+
