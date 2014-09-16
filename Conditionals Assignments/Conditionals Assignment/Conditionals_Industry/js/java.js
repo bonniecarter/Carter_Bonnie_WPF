8d8c14366b7e07dca6258d98655821a91e9a65fd
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-16 15:09:21 -0400

    working on variables
    working on variables for real estate

--- Conditionals Assignments/Conditionals Assignment/Conditionals_Industry/js/java.js
+++ Conditionals Assignments/Conditionals Assignment/Conditionals_Industry/js/java.js
@@ -5,3 +5,16 @@
  * How Many Agents?
  */
 
+// average price of home
+var averagePriceHome = prompt ("What is the average sale price of homes in your area?");
+
+// if each realtor sells ??? houses
+var realtorSells = prompt("How many houses does the average Realtor sell?");
+
+// average commission splits
+var averageCommissionSplit = prompt ("What is the average commission in the purchase & sales of a home?");
+
+// math, what is the commission?
+
+
+
