acbe5f5ae2bb0522ccb68e8246907226c2bb6f4a
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-16 13:00:20 -0400

    Corrected Code
    Simplified & Corrected Code.

--- Conditionals_worksheet/Group 3/js/java.js
+++ Conditionals_worksheet/Group 3/js/java.js
@@ -7,31 +7,25 @@
 
 //determine age (55+ or 10-) $7.00 other is $12.00
 
-var age = ("What is your age?");
+var age = prompt("What is your age?");
 
 //determine time of movie (3-5pm) is additional discount (25% off)
 
-var showTime = ("What time is the show? \n All shows are in PM, no need to type PM.");
+var showTime = prompt("What time is the show? \n All shows are in PM, no need to type PM.");
 
 //determine age & price
 
-if (9 < age < 55 && 3 <= showTime <= 5){
-
-  alert("The ticket price is $9.00")
-}
-
-if (9 < age < 55 && (!(3 <= showTime <= 5))){
-
-    alert("The ticket price is $12")
-}
-
-if ((!(9 < age < 55) )&& 3 <= showTime <= 5){
-
+if (age >= 10 && age <=54){
+    if (showTime >= 3 && showTime <= 5)
+    alert("The ticket price is $9.00")
+    else
+    alert("The ticket price is $12.00")
+}else{
+    if (showTime >= 3 && showTime <= 5)
     alert("The ticket price is $5.25")
+    else
+    alert("The ticket price is 7.00")
 }
 
-if ((!(9 < age < 55)) && (!(3 <= showTime <= 5))){
 
-    alert("The ticket price is $12")
-}
 
