bc96e59db1e124ed5ba355f9cf7a2afb40d5806e
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-22 18:25:32 -0400

    Calculations
    working on calculations of bees

--- Functions_worksheet/Functions Worksheet/Stung/Functions_Circumference/js/java.js
+++ Functions_worksheet/Functions Worksheet/Stung/Functions_Circumference/js/java.js
@@ -4,3 +4,16 @@
  * Assignment Functions Stung!
  */
 
+function beeCalculation(lbs) {
+    var beeStings = 8.666666667;
+    var beeTotal = lbs * beeStings;
+
+    return beeTotal;
+
+    alert("It takes " +beeTotal+ "bee stings to kill this animal.");
+}
+
+var lbAsk = prompt("How many lbs is the animal?");
+var total = beeCalculation(lbAsk);
+
+console.log(total);
