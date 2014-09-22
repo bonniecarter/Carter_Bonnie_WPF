94d695440cb34d211582426d5698560afaf7a6c2
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-22 17:00:36 -0400

    Attempt Circum
    attempting a calculation that prints correctly and calls correctly

--- Functions_worksheet/Functions Worksheet/Circumference/Functions_Circumference/js/java.js
+++ Functions_worksheet/Functions Worksheet/Circumference/Functions_Circumference/js/java.js
@@ -13,3 +13,15 @@
   alert("The circumference is "+circle+".")
 }
  */
+
+var radius = prompt("Enter the radius of the circle:");
+var circumIs = ("the circumference of the circle is " +circle+ ".");
+
+function calcCircum(radius){
+    var circle;
+    circle = 2 * Math.PI * radius;
+
+    return circle;
+}
+
+console.log(circumIs);
