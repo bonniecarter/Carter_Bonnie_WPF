6a7d96ec14b7bf2fa04d8848f561038130274bce
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-22 19:54:06 -0400

    prompts
    creating month day prompts for industry

---
+++ Functions_Assignment/Functions Week 4/Industry/js/java.js
@@ -0,0 +1,15 @@
+/**
+Bonnie Carter
+September 22, 2014
+Week 4
+Industry
+How much $ does the buyer owe the seller for prorated taxes if the home is sold in month x.
+*/
+
+var month = prompt("Your 2014 Closing: \nWhat month are you closing in?");
+var day = prompt("Your " +month+ " 2014 closing: \nWhat day are you closing on?");
+
+function amountOwed() {
+
+    console.log()
+}
---
+++ Functions_Assignment/Functions Week 4/Personal/js/java.js
@@ -0,0 +1,8 @@
+/**
+Bonnie Carter
+September 22, 2014
+Week 4
+Personal
+Idea: ???
+*/
+
