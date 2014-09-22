131bb8e5641cd4c8a49b8f940821d7dd1ebe542c
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-22 17:52:46 -0400

    Tested & Final Save
    Fixed code, tested and double checked math on another calculator.

--- Functions_worksheet/Functions Worksheet/Circumference/Functions_Circumference/js/java.js
+++ Functions_worksheet/Functions Worksheet/Circumference/Functions_Circumference/js/java.js
@@ -14,10 +14,13 @@
 }
  */
 
-var radius = prompt("Enter the radius of the circle","");
-
-function circumference(radius) {
-
-    var circle = 2 * Math.PI * radius;
-    alert("The circumference is "+circle+".")
+function calcCircum(radius){  //create function calcCirc and parameter radius
+    var circle = 2 * Math.PI * radius;  //create calculation for the diameter 2*pi*r
+    alert("The circumference of the circle is: "+circle+""); //alert the user
+    return circle; //return the circle outside of the function to the "total" variable
 }
+
+var radiusAsk = prompt("What is the radius of your circle?"); //create a prompt to hold a variable & insert...
+var total = calcCircum(radiusAsk); //...insert variable into calcCircum function into the radius variable
+
+console.log(total); //print the total circumference from the calcCircum function
