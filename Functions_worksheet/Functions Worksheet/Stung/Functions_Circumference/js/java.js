53cd2e7dbfca60d9340c3cf01f3252e5121c1ac7
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-22 18:28:49 -0400

    comments
    added comments

--- Functions_worksheet/Functions Worksheet/Stung/Functions_Circumference/js/java.js
+++ Functions_worksheet/Functions Worksheet/Stung/Functions_Circumference/js/java.js
@@ -4,10 +4,10 @@
  * Assignment Functions Stung!
  */
 
-function beeCalculation(lbs) {
-    var beeStings = 8.666666667;
-    var beeTotal = lbs * beeStings;
-    alert("It takes " +beeTotal+ "bee stings to kill this animal.");
+function beeCalculation(lbs) {     //created function called beeCalculation that holds the lbs parameter
+    var beeStings = 8.666666667;       //it takes 8.666666667 bee stings per pound
+    var beeTotal = lbs * beeStings;          //getting the total lbs times stings
+    alert("It takes " +beeTotal+ "bee stings to kill this animal.");   //alert user of bee total
 
     return beeTotal;
 }
