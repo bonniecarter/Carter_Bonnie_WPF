f64f5acbc5dbab2b29c41af6875c0ff10a1176a8
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-23 10:06:48 -0400

    Personal
    created initial input of measurement

--- Functions_Assignment/Functions Week 4/Personal/js/java.js
+++ Functions_Assignment/Functions Week 4/Personal/js/java.js
@@ -3,6 +3,23 @@ Bonnie Carter
 September 22, 2014
 Week 4
 Personal
-Idea: ???
+Idea: Kitchen Conversions
 */
+
+var initMeasurement = prompt("What is your measurement?\nPlease specify cup, ounce, tbsp or tsp.");
+
+console.log("Initial Measurement Input: "+initMeasurement);
+
+if (initMeasurement.indexOf("cup") ){
+    console.log("It's a cup");
+}else if (initMeasurement.indexOf("ounce")){
+    console.log("It's an ounce");
+}else if (initMeasurement.indexOf("tbsp")){
+    console.log("It's a tablespoon");
+}else if (initMeasurement.indexOf("tsp")){
+
+}else{
+    console.log("Try again, only cup, ounce, tbsp or tsp is accepted")
+}
+
 
