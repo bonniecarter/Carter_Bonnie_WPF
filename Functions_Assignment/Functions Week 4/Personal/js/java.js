561f46d0fa0440177823bf234f3582c4a21576d7
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-23 10:17:19 -0400

    code invalid
    working on alternatives.

--- Functions_Assignment/Functions Week 4/Personal/js/java.js
+++ Functions_Assignment/Functions Week 4/Personal/js/java.js
@@ -10,16 +10,34 @@ var initMeasurement = prompt("What is your measurement?\nPlease specify cup, oun
 
 console.log("Initial Measurement Input: "+initMeasurement);
 
-if (initMeasurement.indexOf("cup") ){
+/** if (initMeasurement.indexOf("cup") ){
     console.log("It's a cup");
 }else if (initMeasurement.indexOf("ounce")){
     console.log("It's an ounce");
 }else if (initMeasurement.indexOf("tbsp")){
     console.log("It's a tablespoon");
 }else if (initMeasurement.indexOf("tsp")){
-
+    console.log("It's a teaspoon");
 }else{
     console.log("Try again, only cup, ounce, tbsp or tsp is accepted")
 }
+ */
+
+if (initMeasurement.indexOf == "cup") {
+    console.log("It's a cup");
+}
+else if (initMeasurement == "ounce") {
+    console.log("It's an ounce");
+}
+else if (initMeasurement == "tbsp") {
+    console.log("It's a tablespoon");
+}
+else if (initMeasurement == "tsp") {
+    console.log("It's a teaspoon");
+}
+else {
+    console.log("Try again, only cup, ounce, tbsp or tsp is accepted");
+}
+
 
 
