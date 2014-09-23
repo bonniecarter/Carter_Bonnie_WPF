27fcaa42f540d80dc85ed0bb1861334d69d3f024
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-23 12:18:13 -0400

    creating wacky what is your signs
    creating variables

--- Functions_Assignment/Functions Week 4/Wacky/js/java.js
+++ Functions_Assignment/Functions Week 4/Wacky/js/java.js
@@ -3,6 +3,15 @@ Bonnie Carter
 September 22, 2014
 Week 4
 Wacky
-Idea: ???
+Idea: Your Signs
 */
 
+var month = prompt("What month were you born in?\nUse numbers only (ex. 03).");
+var day = prompt("What day were you born on?");
+var year = prompt("What year were you born in?");
+
+function zodiac(m,d,y){
+
+}
+
+zodiac(month,day,year);
