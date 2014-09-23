8861d467fd90714a05ea7b1ea4fe4d5916e43271
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-22 20:08:44 -0400

    month to number
    formula for month to number.

--- Functions_Assignment/Functions Week 4/Industry/js/java.js
+++ Functions_Assignment/Functions Week 4/Industry/js/java.js
@@ -10,6 +10,25 @@ var month = prompt("Your 2014 Closing: \nWhat month are you closing in?");
 var day = prompt("Your " +month+ " 2014 closing: \nWhat day are you closing on?");
 var yrTax = prompt ("What were your paid in full taxes for 2014?");
 
+console.log(month,day,yrTax);
+
+month = {
+    'January' : '01',
+    'February' : '02',
+    'March' : '03',
+    'April' : '04',
+    'May' : '05',
+    'June' : '06',
+    'July' : '07',
+    'August' : '08',
+    'September' : '09',
+    'October' : '10',
+    'November' : '11',
+    'December' : '12'
+};
+
+console.log(month,day,yrTax);
+
 function amountOwed(m,d,t) {
 
     var monthlyTax = t/12;
