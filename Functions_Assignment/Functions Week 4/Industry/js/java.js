b4a4f2d724918d318da1697da315a39f2032d98c
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-22 20:40:36 -0400

    functions for month
    have to use month as number only.

--- Functions_Assignment/Functions Week 4/Industry/js/java.js
+++ Functions_Assignment/Functions Week 4/Industry/js/java.js
@@ -7,25 +7,12 @@ How much $ does the buyer owe the seller for prorated taxes if the home is sold 
 */
 
 var month = prompt("Your 2014 Closing: \nWhat month are you closing in?");
-var day = prompt("Your " +month+ " 2014 closing: \nWhat day are you closing on?");
+var day = prompt("Your " +month+ " 2014 closing: \nWhat day are you closing on (Use Number Only)?");
 var yrTax = prompt ("What were your paid in full taxes for 2014?");
 
 console.log(month,day,yrTax);
 
-month = {
-    'January' : '01',
-    'February' : '02',
-    'March' : '03',
-    'April' : '04',
-    'May' : '05',
-    'June' : '06',
-    'July' : '07',
-    'August' : '08',
-    'September' : '09',
-    'October' : '10',
-    'November' : '11',
-    'December' : '12'
-};
+
 
 console.log(month,day,yrTax);
 
