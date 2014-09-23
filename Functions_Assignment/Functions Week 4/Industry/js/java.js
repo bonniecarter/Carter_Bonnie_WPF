b88e03617f3a821a9c7093af3a7215c54d24d258
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-22 20:00:17 -0400

    monthly tax
    breaking down amount owed function

--- Functions_Assignment/Functions Week 4/Industry/js/java.js
+++ Functions_Assignment/Functions Week 4/Industry/js/java.js
@@ -8,8 +8,15 @@ How much $ does the buyer owe the seller for prorated taxes if the home is sold 
 
 var month = prompt("Your 2014 Closing: \nWhat month are you closing in?");
 var day = prompt("Your " +month+ " 2014 closing: \nWhat day are you closing on?");
+var yrTax = prompt ("What were your paid in full taxes for 2014?");
 
-function amountOwed() {
+function amountOwed(m,d,t) {
+
+    var monthlyTax = t/12;
+
+    return taxTotal;
 
     console.log()
 }
+
+var total = amountOwed(month,day,yrTax);
