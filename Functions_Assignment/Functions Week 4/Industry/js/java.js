83e472dc6f2f73312d6c7e0322d385f22c861a48
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-09-22 21:00:09 -0400

    creating days in month tax for each property

--- Functions_Assignment/Functions Week 4/Industry/js/java.js
+++ Functions_Assignment/Functions Week 4/Industry/js/java.js
@@ -6,23 +6,31 @@ Industry
 How much $ does the buyer owe the seller for prorated taxes if the home is sold in month x.
 */
 
-var month = prompt("Your 2014 Closing: \nWhat month are you closing in?");
-var day = prompt("Your " +month+ " 2014 closing: \nWhat day are you closing on (Use Number Only)?");
+var month = prompt("Your 2014 Closing: \nWhat month are you closing in (Use Number Only)?");
+var day = prompt("Your " +month+ " 2014 closing: \nWhat day are you closing on?");
 var yrTax = prompt ("What were your paid in full taxes for 2014?");
-
-console.log(month,day,yrTax);
-
-
 
 console.log(month,day,yrTax);
 
 function amountOwed(m,d,t) {
 
-    var monthlyTax = t/12;
+    var wholeMonTax = (t/12) * (12-month);
+
+    if (month == 1 || 3 || 5 || 7 || 8 || 10 ||12){
+        var daysOfMonTax = (wholeMonTax/31)(31-d);
+    }else{
+        if (month == 2){
+            daysOfMonTax = (wholeMonTax/28)(28-d);
+        }else{
+            daysOfMonTax = (wholeMonTax/30)(30-d);
+        }
+
+    }
+
+    console.log("month " +m+ ", day " +d+ ", tax for year " +t+ ".");
+    console.log("taxes per month" +wholeMonTax+ ", taxable amount in current month " +daysOfMonTax);
 
     return taxTotal;
-
-    console.log()
 }
 
 var total = amountOwed(month,day,yrTax);
